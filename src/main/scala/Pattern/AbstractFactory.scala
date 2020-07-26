26db56a62d0d9638825c4b3590d22f231a5c5a43
Dima <johndoe@examp>
2020-07-26 20:15:06 +0300

    1. Добавлен паттерн AbstractFactory

---
+++ src/main/scala/Pattern/AbstractFactory.scala
@@ -0,0 +1,16 @@
+package Pattern
+
+class AbstractFactory extends Pattern {
+
+  override  var name: String = "Abstract factory"
+
+  override  var category: String = "Порождающий шаблон проектирования"
+
+  override  var description: String = "Шаблон реализуется созданием абстрактного класса Factory, " +
+    "который представляет собой интерфейс для создания компонентов системы, например, " +
+    "для оконного интерфейса он может создавать окна и кнопки. Затем пишутся классы," +
+    " реализующие этот интерфейс"
+
+}
+
+
