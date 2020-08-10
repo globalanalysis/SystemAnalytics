5b8567a8fb00a6a549c942cdda6ce9d8c8ef3d87
Dima <johndoe@examp>
2020-08-10 21:08:59 +0300

    Добавлены 2 новых шаблона

---
+++ src/main/scala/SubjectModel/PatternClass/Bridge.scala
@@ -0,0 +1,24 @@
+
+/** == Шаблон Bridge==
+  * @author Dynamic
+  * @return name, category, description
+  * @constructor отсуствует
+  **/
+
+
+package SubjectModel.PatternClass
+
+import SubjectModel.Pattern
+
+class Bridge extends Pattern {
+
+  override  var name: String = "Bridge"
+
+  override  var category: String = "Структурные паттерны"
+
+  override  var description: String = "Мост (Bridge) - структурный шаблон проектирования, который позволяет отделить абстракцию от реализации таким образом, чтобы и абстракцию, и реализацию можно было изменять независимо друг от друга."
+
+
+}
+
+
---
+++ src/main/scala/SubjectModel/PatternClass/Facade.scala
@@ -0,0 +1,23 @@
+
+/** == Шаблон Facade==
+  * @author Dynamic
+  * @return name, category, description
+  * @constructor отсуствует
+  **/
+
+
+package SubjectModel.PatternClass
+
+import SubjectModel.Pattern
+
+class Facade extends Pattern {
+
+  override  var name: String = "Facade"
+
+  override  var category: String = "Структурные паттерны"
+
+  override  var description: String = "Фасад (Facade) представляет шаблон проектирования, который позволяет скрыть сложность системы с помощью предоставления упрощенного интерфейса для взаимодействия с ней."
+
+}
+
+
